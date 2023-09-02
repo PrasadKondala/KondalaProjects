07393d0082ec9757aae31cff4f0bc612a5bd80c5
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-09-02 10:05:49 +0530

    Add files via upload

---
+++ equal.js
@@ -0,0 +1,14 @@
+const  num1=10
+const num2=10
+
+const isEqual=num1==num2;
+const isNotEqual=num1 !=num2;
+
+console.log('Are they equal? ${isEqual}');
+console.log('Are they  not equal? ${isNotEqual}');
+
+const isTrue= true;
+const isFalse=false;
+
+console.log('istrue is${isTrue}');
+console.log('isFalse is${isFalse}');
