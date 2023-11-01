fa1950906274d5f48ca0e8e24c6350891c1a73b7
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-01 15:17:15 +0530

    Add files via upload

---
+++ return.js
@@ -0,0 +1,12 @@
+// Function that calculates the square of a number and returns the result
+function calculateSquare(number) {
+    var square = number * number;
+    return square; // Using the return keyword to specify the value to be returned
+  }
+  
+  // Call the function and store the result in a variable
+  var result = calculateSquare(5);
+  
+  // Display the result
+  console.log("The square of 5 is: " + result);
+  
