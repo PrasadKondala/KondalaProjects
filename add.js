8c90bb86debfed0ef75afad0409c6b3a55393d67
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-01 14:32:15 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,13 @@
+// Define two numbers for addition
+var num1 = 5;
+var num2 = 10;
+
+// Perform addition
+var sum = num1 + num2;
+
+// Display the result
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+
+
+
