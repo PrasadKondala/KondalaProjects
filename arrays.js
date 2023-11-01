811fd96af3e1ee7bf5c012a384d2cf5744664c7d
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-01 19:09:20 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,20 @@
+// Creating an array of numbers
+const numbers = [1, 2, 3, 4, 5];
+
+// Accessing elements by index
+console.log(numbers[0]); // Output: 1
+console.log(numbers[2]); // Output: 3
+
+// Modifying an element
+numbers[1] = 10;
+console.log(numbers); // Output: [1, 10, 3, 4, 5]
+
+// Adding elements to the end of the array
+numbers.push(6);
+console.log(numbers); // Output: [1, 10, 3, 4, 5, 6]
+
+// Removing the last element
+numbers.pop();
+console.log(numbers); // Output: [1, 10, 3, 4, 5]
+
+// Getting the length of
