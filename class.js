641d9c9af9582edaa4d5bfb5500dd4cc2df16521
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-01 18:12:47 +0530

    Add files via upload

--- class.js
+++ class.js
@@ -1,6 +1,18 @@
-class text{
-    constructor(){
-        console.log("i am prasad");
+class Person {
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
     }
-}
-new text();
+  
+    getFullName() {
+      return this.firstName + ' ' + this.lastName;
+    }
+  }
+  
+  // Create an instance of the Person class
+  const person1 = new Person('prasad', 'kondala');
+  
+  // Access the properties and methods of the instance
+  console.log(person1.firstName); // Output: John
+  console.log(person1.getFullName()); // Output: John Doe
+  
