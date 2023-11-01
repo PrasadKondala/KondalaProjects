6faf21a18b3f9712dc3559993d15aa6f6a78417f
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-01 15:15:12 +0530

    Add files via upload

---
+++ declare.js
@@ -0,0 +1,18 @@
+// Declare variables
+var x = 5; // Integer variable
+var y = 3.14; // Floating-point variable
+var name = "John"; // String variable
+var isStudent = true; // Boolean variable
+
+var sum = x + y;
+var greeting = "Hello, " + name + "!";
+var age = isStudent ? "You are a student." : "You are not a student.";
+
+console.log("x: " + x);
+console.log("y: " + y);
+console.log("name: " + name);
+console.log("isStudent: " + isStudent);
+
+console.log("Sum of x and y: " + sum);
+console.log(greeting);
+console.log(age);
