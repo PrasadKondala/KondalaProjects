8c340df2be6a67761f771ca5139af68cc27178e1
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-01 14:31:42 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,29 @@
+// Declare integer variables
+var num1 = 10;
+var num2 = 5;
+
+// Addition
+var sum = num1 + num2;
+console.log("Sum: " + sum);
+
+// Subtraction
+var difference = num1 - num2;
+console.log("Difference: " + difference);
+
+// Multiplication
+var product = num1 * num2;
+console.log("Product: " + product);
+
+// Division
+var quotient = num1 / num2;
+console.log("Quotient: " + quotient);
+
+// Modulo (remainder)
+var remainder = num1 % num2;
+console.log("Remainder: " + remainder);
+
+// Increment and decrement
+num1++;
+num2--;
+console.log("Incremented num1: " + num1);
+console.log("Decremented num2: " + num2);
