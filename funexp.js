8f0a55b5d6e381afa2e6b1f3892e5bfb8bffaf77
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-01 15:17:43 +0530

    Add files via upload

---
+++ funexp.js
@@ -0,0 +1,11 @@
+// Function expression to calculate the square of a number
+var calculateSquare = function(number) {
+    return number * number;
+  };
+  
+  // Call the function and store the result in a variable
+  var result = calculateSquare(5);
+  
+  // Display the result
+  console.log("The square of 5 is: " + result);
+  
