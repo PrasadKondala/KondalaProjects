2687f726836249e4df75f9aa3e28895ef1f2be71
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-01 14:33:47 +0530

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,16 @@
+// Declare a boolean variable
+var isTrue = true;
+var isFalse = false;
+
+// Use boolean variables in conditional statements
+if (isTrue) {
+  console.log("isTrue is true");
+} else {
+  console.log("isTrue is false");
+}
+
+if (isFalse) {
+  console.log("isFalse is true");
+} else {
+  console.log("isFalse is false");
+}
---
+++ multiply.js
@@ -0,0 +1,9 @@
+// Define two numbers for multiplication
+var num1 = 5;
+var num2 = 10;
+
+// Perform multiplication
+var product = num1 * num2;
+
+// Display the result
+console.log("The product of " + num1 + " and " + num2 + " is: " + product);
