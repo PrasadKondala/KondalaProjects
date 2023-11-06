60fe24d51b7f109033c5ca58147cd42cbc95a1dc
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-06 15:02:25 +0530

    Add files via upload

---
+++ multi.js
@@ -0,0 +1,14 @@
+// Function to multiply two numbers
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// Input numbers
+const num1 = 5;
+const num2 = 7;
+
+// Calculate the product
+const result = multiplyNumbers(num1, num2);
+
+// Display the result
+console.log(`The product of ${num1} and ${num2} is: ${result}`);
