af34e43aba22a986a9c100d0744058eec78cb61f
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-06 16:15:28 +0530

    Add files via upload

---
+++ natural.js
@@ -0,0 +1,12 @@
+function printNaturalNumbers(limit) {
+    for (let i = 1; i <= limit; i++) {
+      console.log(i);
+    }
+  }
+  
+  // Specify the limit to print natural numbers up to
+  const limit = 19; // You can change this to any positive integer
+  
+  // Call the function to print natural numbers
+  printNaturalNumbers(limit);
+  
