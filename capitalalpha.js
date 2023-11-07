79e3b5bd50cae9a277c2534d4cb399af3b76d2ca
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-07 12:31:28 +0530

    Add files via upload

---
+++ capitalalpha.js
@@ -0,0 +1,7 @@
+// Loop from the ASCII value of 'A' (65) to 'Z' (90)
+for (var i = 65; i <= 90; i++) {
+    // Convert the ASCII value to a character and print it
+    var letter = String.fromCharCode(i);
+    console.log(letter);
+  }
+  
