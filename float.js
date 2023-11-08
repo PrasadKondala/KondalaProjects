0916cbbee3b784ed507dc3fa2e4ccb515e9a690e
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-08 10:55:38 +0530

    Add files via upload

---
+++ float.js
@@ -0,0 +1,14 @@
+// Define some floating-point numbers
+const pi = 3.14159265359;
+const gravity = 9.81;
+const price = 19.99;
+
+// Perform arithmetic operations with floating-point numbers
+const radius = 5.5;
+const area = Math.PI * (radius ** 2);
+
+// Display the results
+console.log(`The value of pi is approximately: ${pi}`);
+console.log(`The acceleration due to gravity is: ${gravity} m/s²`);
+console.log(`The price of the item is: $${price}`);
+console.log(`The area of a circle with a radius of ${radius} units is: ${area.toFixed(2)} square units`);
