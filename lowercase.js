9411820c0c3628773e1e71a2be9c6557c2c0d185
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-08 10:58:28 +0530

    Add files via upload

---
+++ lowercase.js
@@ -0,0 +1,9 @@
+// Define a string with some uppercase characters
+const originalString = "This Is A Mixed Case String";
+
+// Convert the string to lowercase
+const lowercaseString = originalString.toLowerCase();
+
+// Display the original and lowercase strings
+console.log("Original String:", originalString);
+console.log("Lowercase String:", lowercaseString);
