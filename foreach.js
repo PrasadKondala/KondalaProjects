cab6d722d7fafe667004cba033d386c91e096dbf
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-09 16:53:07 +0530

    Add files via upload

---
+++ foreach.js
@@ -0,0 +1,6 @@
+// JavaScript iteration example with forEach
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(function(number) {
+    console.log(number);
+});
---
+++ uppercase.js
@@ -0,0 +1,6 @@
+// JavaScript program to convert a string to uppercase
+const originalString = "hello, world!";
+
+const upperCaseString = originalString.toUpperCase();
+
+console.log(upperCaseString);
