cb43a8ad74aff0e0acc927d55d8886fbb2721d1c
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-25 20:51:05 +0530

    Add files via upload

---
+++ arraysplice.js
@@ -0,0 +1,7 @@
+let prime_numbers = [2, 3, 5, 7, 9, 11];
+
+// replace 1 element from index 4 by 13
+let removedElement = prime_numbers.splice(4, 1, 13);
+console.log(removedElement);
+console.log(prime_numbers);
+
