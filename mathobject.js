1f1d1ce4429786a44250c8df4ce3e8248ab213d8
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-12-02 20:46:08 +0530

    Add files via upload

---
+++ mathobject.js
@@ -0,0 +1,25 @@
+// Example usage of Math object
+
+// Constants
+console.log('PI:', Math.PI); // Displays the value of PI
+
+// Basic Math operations
+console.log('Square root of 16:', Math.sqrt(16)); // Calculates the square root of a number
+console.log('Absolute value of -8:', Math.abs(-8)); // Returns the absolute (positive) value of a number
+
+// Rounding numbers
+console.log('Round 4.7:', Math.round(4.7)); // Rounds to the nearest integer
+console.log('Ceiling of 4.2:', Math.ceil(4.2)); // Rounds up to the nearest integer
+console.log('Floor of 4.8:', Math.floor(4.8)); // Rounds down to the nearest integer
+
+// Generating random numbers
+console.log('Random number between 0 and 1:', Math.random()); // Generates a random number between 0 (inclusive) and 1 (exclusive)
+console.log('Random number between 1 and 10:', Math.floor(Math.random() * 10) + 1); // Generates a random integer between 1 and 10
+
+// Trigonometric functions (in radians)
+console.log('Sine of 45 degrees:', Math.sin(Math.PI / 4)); // Calculates the sine of an angle
+console.log('Cosine of 60 degrees:', Math.cos(Math.PI / 3)); // Calculates the cosine of an angle
+
+// Exponential and logarithmic functions
+console.log('e raised to power 2:', Math.exp(2)); // Returns e raised to the power of a number
+console.log('Natural logarithm of 10:', Math.log(10)); // Returns the natural logarithm of a number
