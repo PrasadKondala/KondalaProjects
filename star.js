5b35eb2affe1f2c4246215ca4ec754aee290de75
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-12-04 19:46:17 +0530

    Add files via upload

---
+++ star.js
@@ -0,0 +1,17 @@
+function drawStar() {
+    const star = 
+    `
+        *
+     *     *
+   *         *
+  *************
+   *       *
+     *   *
+       *
+    `;
+    console.log(star);
+  }
+  
+  // Call the function to draw the star
+  drawStar();
+  
