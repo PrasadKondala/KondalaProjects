a7daa752dc1fe035c863f21e2da579d3a81934f8
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-12-06 19:37:51 +0530

    Add files via upload

---
+++ dtypes.js
@@ -0,0 +1,41 @@
+// String
+let myString = 'Hello, World!';
+
+// Number
+let myNumber = 42;
+
+// Boolean
+let isTrue = true;
+let isFalse = false;
+
+// Array
+let myArray = [1, 2, 3, 4, 5];
+let mixedArray = ['apple', 6, true, { key: 'value' }];
+
+// Object
+let myObject = {
+  name: 'John',
+  age: 30,
+  isStudent: false
+};
+
+// Function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Null
+let myNull = null;
+
+// Undefined
+let myUndefined;
+
+// Logging the data types
+console.log('String:', myString);
+console.log('Number:', myNumber);
+console.log('Boolean:', isTrue, isFalse);
+console.log('Array:', myArray, mixedArray);
+console.log('Object:', myObject);
+console.log('Function:', greet('Alice'));
+console.log('Null:', myNull);
+console.log('Undefined:', myUndefined);
