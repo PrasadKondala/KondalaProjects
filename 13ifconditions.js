6c21ed1abadfb4dd4604625b1796b600746fa42b
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2024-01-04 12:12:34 +0530

    Add files via upload

---
+++ 13ifconditions.js
@@ -0,0 +1,13 @@
+// Define a variable
+let temperature = 25;
+
+// Check the temperature and provide a message
+if (temperature < 0) {
+  console.log("It's freezing outside!");
+} else if (temperature >= 0 && temperature < 20) {
+  console.log("It's cold outside.");
+} else if (temperature >= 20 && temperature < 30) {
+  console.log("It's a nice day.");
+} else {
+  console.log("It's hot outside!");
+}
